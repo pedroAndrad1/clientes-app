d12bac1bf407f476e4e3f9d86dee72315e4c1e7e
Pedro <pedropaulo3521@gmail.com>
2020-09-30 20:17:58 -0300

    Componetizado o sidevav

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    SidenavComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    SidenavComponent
   ]
 })
 export class TemplateModule { }
