d0ef4c46067d835e92f1f38bbbc53f6689f15ca9
Pedro <pedropaulo3521@gmail.com>
2020-09-30 20:31:50 -0300

    Componentizado o footer

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    SidenavComponent
+    SidenavComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavbarComponent,
-    SidenavComponent
+    SidenavComponent,
+    FooterComponent
   ]
 })
 export class TemplateModule { }
