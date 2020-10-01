cfc10f4b5610ec71aa757f1a07c72b6b1ac2ab01
Pedro <pedropaulo3521@gmail.com>
2020-10-01 19:23:48 -0300

    Estilizado o formulário de cadastro de clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component';
+import { ClientesModule } from './clientes/clientes.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TemplateModule
+    TemplateModule,
+    ClientesModule
     
   ],
   providers: [],
