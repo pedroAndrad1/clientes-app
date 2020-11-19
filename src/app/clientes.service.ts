bdb5cdcb3e0d51e4563f8a8c3c777166fe8cdfff
Pedro <pedropaulo3521@gmail.com>
2020-11-19 17:04:02 -0300

    Adicionado variáveis de ambiente

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Cliente } from './clientes/cliente';
 
 @Injectable({
@@ -8,25 +9,27 @@ import { Cliente } from './clientes/cliente';
 })
 export class ClientesService {
 
+  API_URL = environment.API_BASE + "/api/clientes"
+
   constructor(private httpClient: HttpClient) { }
 
   save(cliente: Cliente): Observable<Cliente>{
-      return this.httpClient.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+      return this.httpClient.post<Cliente>(this.API_URL, cliente);
   }
 
   update(cliente: Cliente): Observable<Cliente>{
-    return this.httpClient.put<Cliente>(`http://localhost:8080/api/clientes/${cliente.id}`, cliente);
+    return this.httpClient.put<Cliente>(`${this.API_URL}/${cliente.id}`, cliente);
 }
 
   getClientes(): Observable<Cliente[]>{
-    return this.httpClient.get<Cliente[]>("http://localhost:8080/api/clientes");
+    return this.httpClient.get<Cliente[]>(this.API_URL);
   }
 
   getClienteById(id: number): Observable<Cliente>{
-    return this.httpClient.get<Cliente>(`http://localhost:8080/api/clientes/${id}`);
+    return this.httpClient.get<Cliente>(`${this.API_URL}/${id}`);
   }
   
   delete(id: Number): Observable<any>{
-    return this.httpClient.delete<any>(`http://localhost:8080/api/clientes/${id}`);
+    return this.httpClient.delete<any>(`${this.API_URL}/${id}`);
   }
 }
