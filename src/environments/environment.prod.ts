5c549e0e069d10f15fd0edcead70c050383c4bb4
Pedro <pedropaulo3521@gmail.com>
2021-01-04 14:06:52 -0300

    Adicionada a url da API em produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  API_BASE: "https://clientes-api-projeto.herokuapp.com",
+  TOKEN_URL:"/oauth/token",
+  CLIENT_ID:"app-vendas-pedro-andrade",
+  CLIENT_SECRET: "acai-com-leite-em-po"
 };
